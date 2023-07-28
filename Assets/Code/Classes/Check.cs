c2090f21ffb436e7210a64fd8bd46f37cccbc201
msand67 <msand67@wgu.edu>
2023-07-28 11:19:07 -0600

    Added isRequired flag to Check
    Also removed timeToExecutre from CheckResult return value. That should be tracked by the manager.

--- Assets/Code/Classes/Check.cs
+++ Assets/Code/Classes/Check.cs
@@ -9,32 +9,35 @@ namespace dataStructures
         public CheckType type;
         public CheckDifficulty difficulty;
         public FailPenalty penalty;
-        bool isHidden;
-        double timeToExecute;
-        public Check(CheckType iType = CheckType.Breach, int iDifficulty=10, int iPenalty=2, bool iIsHidden = false, double iTimeToExecute = 10.0)
+        public bool isHidden;
+        public double timeToExecute;
+        public bool isRequired;
+        public Check(CheckType iType = CheckType.Breach, int iDifficulty=10, int iPenalty=2, bool iIsHidden = false, double iTimeToExecute = 10.0, bool iIsRequired = false)
         {
             type = iType;
             difficulty = (CheckDifficulty)iDifficulty;
             penalty = (FailPenalty)iPenalty;
             isHidden = iIsHidden;
             timeToExecute = iTimeToExecute;
+            isRequired = iIsRequired;
         }
-        public Check(CheckType iType = CheckType.Breach, CheckDifficulty iDifficulty=CheckDifficulty.easy, FailPenalty iPenalty = FailPenalty.medium, bool iIsHidden = false, double iTimeToExecute = 10.0)
+        public Check(CheckType iType = CheckType.Breach, CheckDifficulty iDifficulty=CheckDifficulty.easy, FailPenalty iPenalty = FailPenalty.medium, bool iIsHidden = false, double iTimeToExecute = 10.0, bool iIsRequired =false)
         {
             type = iType;
             difficulty = iDifficulty;
             penalty = iPenalty;
             isHidden = iIsHidden;
             timeToExecute = iTimeToExecute;
+            isRequired = iIsRequired;
         }
 
-        public (bool, int, double) PerformCheck(int attemptValue, int difficultyMod = 0)
+        public (bool, int) PerformCheck(int attemptValue, int difficultyMod = 0)
         {
             if (attemptValue >= (int)difficulty + difficultyMod)
             {
-                return (true, 0, timeToExecute);
+                return (true, 0);
             }
-            return (false, (int)penalty, timeToExecute);
+            return (false, (int)penalty);
         }
 
     }
