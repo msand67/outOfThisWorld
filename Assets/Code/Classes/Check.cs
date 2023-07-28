adf2bbbc19b6fbb8f701bd29dbeb1609571f878e
msand67 <msand67@wgu.edu>
2023-07-28 11:05:37 -0600

    Add CheckTypes and TimeToExecute
    Adds the stat types and check types. Includes time to execute in the return of CheckResult. Lastly, reformats all enum name casing.

--- Assets/Code/Classes/Check.cs
+++ Assets/Code/Classes/Check.cs
@@ -6,31 +6,35 @@ namespace dataStructures
 {
     public class Check
     {
-        public checkDifficulty difficulty;
-        public failPenalty penalty;
-        public Check()
+        public CheckType type;
+        public CheckDifficulty difficulty;
+        public FailPenalty penalty;
+        bool isHidden;
+        double timeToExecute;
+        public Check(CheckType iType = CheckType.Breach, int iDifficulty=10, int iPenalty=2, bool iIsHidden = false, double iTimeToExecute = 10.0)
         {
-            difficulty = checkDifficulty.easy;
-            penalty = failPenalty.medium;
+            type = iType;
+            difficulty = (CheckDifficulty)iDifficulty;
+            penalty = (FailPenalty)iPenalty;
+            isHidden = iIsHidden;
+            timeToExecute = iTimeToExecute;
         }
-        public Check(int iDifficulty, int iPenalty)
+        public Check(CheckType iType = CheckType.Breach, CheckDifficulty iDifficulty=CheckDifficulty.easy, FailPenalty iPenalty = FailPenalty.medium, bool iIsHidden = false, double iTimeToExecute = 10.0)
         {
-            difficulty = (checkDifficulty)iDifficulty;
-            penalty = (failPenalty)iPenalty;
-        }
-        public Check(checkDifficulty iDifficulty, failPenalty iPenalty)
-        {
+            type = iType;
             difficulty = iDifficulty;
             penalty = iPenalty;
+            isHidden = iIsHidden;
+            timeToExecute = iTimeToExecute;
         }
 
-        public (bool, int) PerformCheck(int attemptValue, int difficultyMod = 0)
+        public (bool, int, double) PerformCheck(int attemptValue, int difficultyMod = 0)
         {
             if (attemptValue >= (int)difficulty + difficultyMod)
             {
-                return (true, 0);
+                return (true, 0, timeToExecute);
             }
-            return (false, (int)penalty);
+            return (false, (int)penalty, timeToExecute);
         }
 
     }
--- Assets/Code/Classes/DataTypes.cs
+++ Assets/Code/Classes/DataTypes.cs
@@ -6,19 +6,19 @@ namespace dataStructures
 {
 
     //DC of a check
-    public enum checkDifficulty
+    public enum CheckDifficulty
     {
-        pitiful=7,
-        easy=10,
-        approachable=13,
-        complex=16,
+        pitiful = 7,
+        easy = 10,
+        approachable = 13,
+        complex = 16,
         demanding = 19
     }
 
 
     //securityPenalty is a modifier to the DC of checks made for each consecutive security level.
     //Currently cut in half.
-    public enum securityPenalty 
+    public enum SecurityPenalty
     {
         low = 1,
         medium = 2,
@@ -26,19 +26,29 @@ namespace dataStructures
     }
 
     //How many seconds are added for a failure after grace period.
-    public enum failPenalty 
+    public enum FailPenalty
     {
-        low= 1,
-        medium=2,
-        high=3
+        low = 1,
+        medium = 2,
+        high = 3
 
     }
 
     //Security interval is how many seconds pass until security level rises.
-    public enum securityInterval //definitely needs to be looked at when balancing
+    public enum SecurityInterval //definitely needs to be looked at when balancing
     {
         low = 5,
         medium = 10,
         high = 15
+    }
+    public enum CheckType
+    {
+        Breach, Egghead, SmoothTalk, Gumshoe,  
+    }
+
+    //first four used for checks, footwork and fastHands modify time to act.
+    public enum StatType
+    {
+        Breach, Egghead, Smoothtalk, Gumshoe, Footwork, FastHands
     }
 }
