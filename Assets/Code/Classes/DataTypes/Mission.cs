a9564ab0851758fa06974764a7c2d0ba004f53a3
msand67 <msand67@wgu.edu>
2023-07-29 16:01:05 -0600

    Update Mission.cs
    Remove agentLocation and plan from this object.

--- Assets/Code/Classes/DataTypes/Mission.cs
+++ Assets/Code/Classes/DataTypes/Mission.cs
@@ -15,32 +15,20 @@ namespace dataStructures
         public double bonusReward;
         public BonusCriteria bonusCriteria;
 
-        public List<AgentLocation> agentLocations; //int is index of what room in the map they are in.
 
-        public Plan myPlan;
-
-        public Mission()
+        public Mission(Map iMyMap)
         {
-            myMap = new Map();
+            myMap = iMyMap;
             gracePeriod = 120;
             securityInterval = SecurityInterval.medium;
             penalty = SecurityPenalty.medium;
             baseReward = 100000;
             bonusReward = 25000;
             bonusCriteria = BonusCriteria.TimeConstraint;
-
-            agentLocations = new List<AgentLocation>();
-
             List<ActionList> tempPlanSteps = new List<ActionList>();
-            foreach(AgentLocation a  in agentLocations)
-            {
-                tempPlanSteps.Add(new ActionList( a.agentId, new List<PlanStep>()));
-            }
 
-
-            myPlan = new Plan(tempPlanSteps);
         }
-        public Mission(Map iMyMap, double iGracePeriod, SecurityInterval iMyInterval, SecurityPenalty iMyPenalty, double iBaseReward, double iBonusReward, BonusCriteria iMyCriteria, List<AgentLocation> iAgentList, Plan iMyPlan)
+        public Mission(Map iMyMap, double iGracePeriod, SecurityInterval iMyInterval, SecurityPenalty iMyPenalty, double iBaseReward, double iBonusReward, BonusCriteria iMyCriteria)
         {
             myMap = iMyMap;
             gracePeriod = iGracePeriod;
@@ -49,29 +37,6 @@ namespace dataStructures
             baseReward = iBaseReward;
             bonusReward = iBonusReward;
             bonusCriteria = iMyCriteria;
-            agentLocations = iAgentList;
-            myPlan = iMyPlan;
-        }
-
-        internal void UpdateLocation(int id, int roomNumber)
-        {
-            foreach(AgentLocation location in agentLocations)
-            {
-                if (location.agentId == id)
-                {
-                    location.roomId = roomNumber;
-                }
-            }
-        }
-    }
-    public class AgentLocation
-    {
-        public int agentId;
-        public int roomId;
-        public AgentLocation(int iAgentId = -1, int iRoomid = -1)
-        {
-            agentId = iAgentId;
-            roomId = iRoomid;
         }
     }
 }
