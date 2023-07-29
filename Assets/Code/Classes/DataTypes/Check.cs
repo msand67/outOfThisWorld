7c6db3472dcf72479d26fa895eba6cb71a4b7d23
msand67 <msand67@wgu.edu>
2023-07-29 15:59:54 -0600

    Update Check.cs
    Add isComplete flack to Check

--- Assets/Code/Classes/DataTypes/Check.cs
+++ Assets/Code/Classes/DataTypes/Check.cs
@@ -12,7 +12,8 @@ namespace dataStructures
         public bool isHidden;
         public double timeToExecute;
         public bool isRequired;
-        public Check(int iType = 1, int iDifficulty=10, int iPenalty=2, bool iIsHidden = false, double iTimeToExecute = 10.0, bool iIsRequired = false)
+        public bool isComplete;
+        public Check(int iType = 1, int iDifficulty=10, int iPenalty=2, bool iIsHidden = false, double iTimeToExecute = 10.0, bool iIsRequired = false, bool iIsComplete=false)
         {
             type = (CheckType)iType;
             difficulty = (CheckDifficulty)iDifficulty;
@@ -20,8 +21,9 @@ namespace dataStructures
             isHidden = iIsHidden;
             timeToExecute = iTimeToExecute;
             isRequired = iIsRequired;
+            isComplete = iIsComplete;
         }
-        public Check(CheckType iType = CheckType.Breach, CheckDifficulty iDifficulty=CheckDifficulty.easy, FailPenalty iPenalty = FailPenalty.medium, bool iIsHidden = false, double iTimeToExecute = 10.0, bool iIsRequired =false)
+        public Check(CheckType iType = CheckType.Breach, CheckDifficulty iDifficulty=CheckDifficulty.easy, FailPenalty iPenalty = FailPenalty.medium, bool iIsHidden = false, double iTimeToExecute = 10.0, bool iIsRequired =false, bool iIsComplete=false)
         {
             type = iType;
             difficulty = iDifficulty;
@@ -29,6 +31,7 @@ namespace dataStructures
             isHidden = iIsHidden;
             timeToExecute = iTimeToExecute;
             isRequired = iIsRequired;
+            isComplete = iIsComplete;
         }
 
         public (bool, int, double) PerformCheck(List<Stat> statList, int difficultyMod = 0)
@@ -36,6 +39,7 @@ namespace dataStructures
             int attemptValue = UnityEngine.Random.Range(0, 20) + statList[(int)type].level;
             if (attemptValue >= (int)difficulty + difficultyMod)
             {
+                isComplete = true;
                 return (true, 0, 0);
             }
             return (false, (int)penalty, timeToExecute);
